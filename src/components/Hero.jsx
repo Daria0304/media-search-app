edd81e09f870e845eb68fbd66f67550d2ca653f2
Daria Szubierajska <daria.szubierajska@gmail.com>
2025-02-04 13:18:05 +0100

    Add tailwind, headlessUI, heroicons, prettier-plugin-tailwindcss libraries & initial components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <header className="p-12">
+      <h1 className="text-xl font-semibold">Main heading</h1>
+      <h2 className="text-l">Sub-heading</h2>
+    </header>
+  );
+};
+
+export default Hero;
